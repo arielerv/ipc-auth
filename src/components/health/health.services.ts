c73f48d96af19a7aa7c2dd0f445ff00a91459d8d
Erviti Ariel <aerviti@indec.gob.ar>
2023-06-22 18:55:17 +0000

    feat: add more status on health

--- src/components/health/health.services.ts
+++ src/components/health/health.services.ts
@@ -1,7 +1,22 @@
 import { GetHealthStatus } from './health.types';
+import fetch from 'node-fetch';
+import { config } from '../../helpers';
 
-export const getHealthStatus = (): GetHealthStatus => {
-    return {
-        status: 200,
-    };
+const API = `${config.ENDPOINT}/`;
+const AUTH_API = `${config.AUTH_ENDPOINT}/`;
+
+export const getHealthStatus = async (): Promise<GetHealthStatus> => {
+    const apiResponse = await fetch(`${API}ping`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const authResponse = await fetch(`${AUTH_API}ping`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    return { app: 200, be: apiResponse.status, arq: authResponse.status };
 };
